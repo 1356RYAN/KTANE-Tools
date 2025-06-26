29167d0ceebc7225b24e24d3571dae01b3a62cbe
1356RYAN <64508604+1356RYAN@users.noreply.github.com>
2025-06-25 21:42:56 -0700

    Rearrange the boxes

--- gui/main.py
+++ gui/main.py
@@ -53,14 +53,11 @@ def main():
     # Gui Section
 
     dpg.create_context()
-    dpg.create_viewport(title='KTANE Tools', width=455, height=455, min_width=455, min_height=455, max_width=455, max_height=455)
+    dpg.create_viewport(title='KTANE Tools', width=465, height=455, min_width=465, min_height=455, max_width=465, max_height=455)
 
     with dpg.window(label="Combined Info", height=220, width=240, pos=(0, 0), no_resize=True, no_move=True, no_collapse=True):
         dpg.add_text("Enter Serial Number:")
         dpg.add_input_text(tag="Serial", default_value="")
-
-        dpg.add_text("Serial Port Present:")
-        dpg.add_checkbox(tag="Serial Port", default_value=False)
 
         dpg.add_text("Enter Amount of Unlit Indicators:")
         dpg.add_input_int(tag="Unlit Total", default_value=0)
@@ -68,40 +65,42 @@ def main():
         dpg.add_text("Enter Amount of Lit Indicators:")
         dpg.add_input_int(tag="Lit Total", default_value=0)
 
+        dpg.add_text("Serial Port Present:")
+        dpg.add_checkbox(tag="Serial Port", default_value=False)
+
     with dpg.window(label="Forget Me Not", height=200, width=240, pos=(0, 220), no_resize=True, no_move=True, no_collapse=True):
+
+        dpg.add_text("Unlit CAR Indicator Present:")
+        dpg.add_checkbox(tag="Unlit Car", default_value=False)
 
         dpg.add_text("Enter Numbers:")
         dpg.add_input_text(tag="Numbers", default_value="")
 
-
-        dpg.add_text("Unlit Indicators (Car):")
-        dpg.add_checkbox(tag="Unlit Car", default_value=False)
-
         
         dpg.add_button(label="Calculate", callback=forget_me_not_call)
 
-        dpg.add_text("Final List:")
+        dpg.add_text("Final Number Order:")
         dpg.add_text("", tag="Final List")
 
-    with dpg.window(label="Slots", height=420, width=200,pos=(240, 0), no_resize=True, no_move=True, no_collapse=True):
+    with dpg.window(label="Slots", height=420, width=210, pos=(240, 0), no_resize=True, no_move=True, no_collapse=True):
 
         dpg.add_text("RCA/PS2 Present:")
         dpg.add_checkbox(tag="RCA/PS2", default_value=False)
 
-        dpg.add_text("Enter Full List of Slots:")
-        dpg.add_input_text(tag="Full List", default_value="")
-
         dpg.add_text("Parallel Present:")
         dpg.add_checkbox(tag="Parallel", default_value=False)
 
-        dpg.add_text("Enter Amount of Batteries:")
-        dpg.add_input_int(tag="Batteries", default_value=0)
-
-        dpg.add_text("Unlit BOB Present:")
+        dpg.add_text("Unlit BOB Indicator Present:")
         dpg.add_checkbox(tag="Unlit BOB", default_value=False)
 
+        dpg.add_text("Enter Amount of Batteries:")
+        dpg.add_input_int(tag="Batteries", default_value=0)
+        
+        dpg.add_text("Enter the Slot Numbers:")
+        dpg.add_input_text(tag="Full List", default_value="")
 
-        dpg.add_button(label="Calculate Slots", callback=slots_call)
+
+        dpg.add_button(label="Calculate", callback=slots_call)
 
         dpg.add_text("Final Slot Numbers:")
         dpg.add_text("", tag="Final Slots")
@@ -114,4 +113,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
